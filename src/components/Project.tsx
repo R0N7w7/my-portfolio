6a618c222976857959a710a2639bf19d52edd782
Roberto Cruz <cr439694@uaeh.edu.mx>
2024-06-27 21:00:03 -0600

    Project links added

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image"
+import Link from "next/link"
 
 type Props = {
     title: string,
     description: string,
+    imageUrl: string,
     url: string
 }
 
-const Project = ({ title, description, url }: Props) => {
+const Project = ({ title, description, imageUrl, url }: Props) => {
     return (
         <article className="bg-white flex flex-col w-full rounded-md p-4 shadow-lg gap-2 max-w-md">
             <h3 className="text-xl font-semibold">{title}</h3>
             <p className="text-lg text-gray-700 font-medium line-clamp-3 overflow-hidden text-ellipsis">{description}</p>
             <div className="w-full flex items-center justify-center md:px-5 ">
                 <Image
-                    src={url}
+                    src={imageUrl}
                     alt={`Imagen ilustrativa de ${title}`}
                     width={2400}
                     height={1350}
@@ -21,7 +23,7 @@ const Project = ({ title, description, url }: Props) => {
                 />
             </div>
 
-            <button className="bg-black shadow-sm text-white rounded px-3 py-2 text-base font-bold hover:bg-gray-900 transition-all max-w-max mx-auto mt-2">Ver Proyecto</button>
+            <Link href={url} className="bg-black shadow-sm text-white rounded px-3 py-2 text-base font-bold hover:bg-gray-900 transition-all max-w-max mx-auto mt-2">Ver Proyecto</Link>
         </article>
     )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import { Project } from "./Project"
 type Projects = {
     title: string,
     description: string,
+    imageUrl: string,
     url: string
 }
 
@@ -10,27 +11,32 @@ const projects: Array<Projects> = [
     {
         title: 'Oftalmología & Cirugía Ocular - Web',
         description: 'Desarrollamos un sitio web desde cero, optimizado para los más altos estándares de SEO, mejorando significativamente el posicionamiento orgánico en los motores de búsqueda más populares. La plataforma ofrece una experiencia de usuario excepcional y está diseñada para destacar en su campo.',
-        url: '/oftalmologia.jpg'
+        imageUrl: '/oftalmologia.jpg',
+        url: "https://oftalmologopachuca.com.mx/"
     },
     {
         title: 'Cirujano de Rodilla | Dr. Marco Jardinez',
         description: 'Creamos una landing page ultrarrápida y eficiente, desarrollada con Next.js y Tailwind CSS. Esta página está diseñada para cargar en tiempo récord, proporcionando una experiencia de usuario fluida y optimizada, ideal para captar la atención de pacientes potenciales.',
-        url: '/rodilla.jpg'
+        imageUrl: '/rodilla.jpg',
+        url: "https://cirujanoderodilla.com.mx/"
     },
     {
         title: 'Uni-Inventario - ERP de Inventarios',
         description: 'Una avanzada plataforma web de control y gestión de inventarios, desarrollada con las tecnologías más modernas: React para el frontend y Node.js en el backend. Utiliza una base de datos MySQL robusta y está optimizada con Express.js para un rendimiento superior. El diseño de la interfaz de usuario es moderno y altamente intuitivo, gracias a la integración de bibliotecas de diseño como Ant Design (antd) y Tailwind CSS. Uni-Inventario garantiza una gestión eficiente y una experiencia de usuario excepcional y fluida.',
-        url: '/uninventario.jpg'
+        imageUrl: '/uninventario.jpg',
+        url: "https://github.com/R0N7w7/inventario-client-jsx"
     },
     {
         title: 'C-Means Calculator',
         description: 'Una herramienta interactiva de cálculo para algoritmos C-Means difuso y no difuso, construida con React.js. Incorpora bibliotecas visuales como Chart.js para ilustrar patrones y comportamientos, ofreciendo una experiencia de usuario altamente intuitiva y dinámica.',
-        url: '/cmeans.jpg'
+        imageUrl: '/cmeans.jpg',
+        url: "https://c-means-typescript.vercel.app/"
     },
     {
         title: 'Gest-All (En progreso...)',
         description: 'Gest-All es un ERP integral diseñado para administrar y gestionar empleados, asistencias y nóminas. Desarrollado desde cero utilizando Node.js y Express.js para el backend, y React.js para el frontend, acompañado de una base de datos MySQL y un diseño moderno con CSS y HTML. Esta plataforma promete revolucionar la eficiencia en la gestión de recursos humanos con su enfoque integral y tecnología de vanguardia.',
-        url: '/gestall.jpg'
+        imageUrl: '/gestall.jpg',
+        url: "https://github.com/R0N7w7/GEST-ALL-backend-"
     }
 ]
 
@@ -43,7 +49,8 @@ const Projects = () => {
                 {projects.map((project, index) => (
                     <Project
                         key={index} description={project.description}
-                        title={project.title} url={project.url}
+                        title={project.title} imageUrl={project.imageUrl}
+                        url={project.url}
                     />
                 ))}
             </div>

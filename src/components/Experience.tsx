4fe9dc2317d02abf14ec3260faf86875ce36f45b
Roberto Cruz <cr439694@uaeh.edu.mx>
2024-09-23 16:32:43 -0600

    fix: Experience data fixed

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,16 +8,10 @@ const Experience = () => {
 
             <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 mt-4 mb-4">
                 <article className="px-3 bg-gray-800 text-white py-4 mx-2 rounded-md max-w-xl">
-                    <h3 className="text-2xl font-medium">Principal Frontend Developer</h3>
-                    <p className="text-lg font-normal mt-2">Quantum</p>
-                    <p className="text-base font-light">Enero 2024 - Actualidad</p>
-                    <p className="text-lg font-light mt-4">Me especializo en la implementación de características visuales que potencian la experiencia del cliente. Dirijo la ejecución de características clave para la startup, garantizando su implementación independientemente del proyecto en cuestión. Mi enfoque incluye una comunicación efectiva y coordinada con el equipo backend para lograr un desarrollo integral y sin fisuras.</p>
-                </article>
-                <article className="px-3 bg-gray-800 text-white py-4 mx-2 rounded-md max-w-xl">
                     <h3 className="text-2xl font-medium">Desarrollador Web Fullstack</h3>
-                    <p className="text-lg font-normal mt-2">Oftalmología & Cirugía Ocular, Pachuca</p>
-                    <p className="text-base font-light">Octubre 2023 - Diciembre 2023</p>
-                    <p className="text-lg font-light mt-4">Responsable del rediseño, desarrollo, implementación y despliegue de aplicaciones web. Implementación de estrategias SEO para mejorar el posicionamiento en buscadores. Liderazgo de equipos pequeños de trabajo, con enfoque en la separación de responsabilidades y el desarrollo ágil.</p>
+                    <p className="text-lg font-normal mt-2">Self Employed, Autonómo</p>
+                    <p className="text-base font-light">Mayo 2023 - Actualidad</p>
+                    <p className="text-lg font-light mt-4">Responsable del rediseño, desarrollo, implementación y despliegue de aplicaciones web con enfoque en el desarrollo backend y frontend empleando tecnologías modernas basadas en el ecosistema de Javascript. Implementación de estrategias SEO para mejorar el posicionamiento en buscadores. Liderazgo de equipos pequeños de trabajo, con enfoque en la separación de responsabilidades y el desarrollo ágil.</p>
                 </article>
             </div>
         </section>
